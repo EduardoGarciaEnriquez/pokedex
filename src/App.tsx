be2f58eb91a2bed6ad7a10da896b3dc05d7d8fcd
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-08-26 10:15:11 -0600

    Feat: Display pokemon name.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import { useEffect } from 'react'
 import Header from './components/header'
+import Card, { IProps } from './components/ui/card'
+import { useGetPokemons } from './hooks/useGetPokemons'
 
 function App() {
+  const { data, loading, error, getPokemons } = useGetPokemons()
+
+  useEffect(() => {
+    getPokemons()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
-    <div className="min-h-[100vh]">
+    <div className="min-h-[100vh] dark:bg-slate-800 bg-slate-200">
       <Header />
+      <div className="py-24 w-full max-w-2xl mx-auto grid gap-4 grid-cols-1 md:grid-cols-2 justify-items-center px-4 md:px-0">
+        {!loading &&
+          !error &&
+          data &&
+          data.map(({ name }: IProps) => {
+            return (
+              <div key={name}>
+                <Card name={name} />
+              </div>
+            )
+          })}
+      </div>
     </div>
   )
 }
