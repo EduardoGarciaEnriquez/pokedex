e40a4cd4030076bb7a70f70a7cc6dd0c6b5d663e
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-08-28 12:46:35 -0600

    Style: added z-index to header.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,7 @@ import ThemeSwitch from '../ui/theme-switch'
 
 function Header() {
   return (
-    <header className=" shadow-md fixed top-0 dark:bg-slate-800 bg-slate-200 left-0 right-0">
+    <header className="shadow-md fixed top-0 dark:bg-slate-800 bg-slate-200 left-0 right-0 z-10">
       <div className="flex justify-between align-middle gap-6 px-4 py-4 max-w-2xl mx-auto">
         <Logo />
         <Search />
