5444fcd46050beb00e4a7360bb052747f4c6f416
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-08-29 21:24:26 -0600

    Style: fade out toast.

--- src/components/toast/index.tsx
+++ src/components/toast/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { closeToast, Roles } from '../../store/slices/pokemonSlice'
 import { AppDispatch, IRootState } from '../../store/store'
@@ -7,6 +8,7 @@ function Toast() {
   const { toastRole, toastMsg, isToastVisible } = useSelector(
     (state: IRootState) => state.pokemon
   )
+  const [dismiss, setDismiss] = useState(false)
 
   const dispatch = useDispatch<AppDispatch>()
 
@@ -24,23 +26,31 @@ function Toast() {
   }
 
   const onClose = () => {
-    dispatch(closeToast())
+    setDismiss(true)
+    setTimeout(() => {
+      dispatch(closeToast())
+      setDismiss(false)
+    }, 1000)
   }
 
   if (!isToastVisible) return <></>
 
   return (
-    <div className="fixed top-20 z-10 w-full flex justify-center ">
+    <div
+      className={`fixed top-20 z-10 w-full flex justify-center duration-1000 ease-in-out transition-all ${
+        dismiss && ' opacity-0'
+      }`}
+    >
       <div
         id="toast"
-        className={`flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg dark:text-gray-400 dark:bg-gray-700 shadow-md border border-${borderColor}-100 transition-all duration-1000 ease-in-out`}
+        className={`flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg dark:text-gray-400 dark:bg-gray-700 shadow-md border border-${borderColor}-100 duration-1000 ease-in-out transition-all`}
         role="alert"
       >
         <ToastIcon />
         <div className="ms-3 text-sm font-normal">{toastMsg}</div>
         <button
           type="button"
-          className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700 transition-all duration-1000 ease-in-out"
+          className="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:hover:bg-gray-800 dark:bg-gray-700 transition-colors duration-1000 ease-in-out"
           data-dismiss-target="#toast"
           aria-label="Close"
           onClick={onClose}
