5bf3435f1593509ae9fc1ef5c1cfcc5a6d81ecaf
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-09-02 09:45:15 -0600

    Feat: Added pokemon types/categories to the drawer menu.

--- src/components/drawer/menu.tsx
+++ src/components/drawer/menu.tsx
@@ -1,8 +1,11 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { toggleDrawer } from '../../store/slices/uiSlice'
-import { AppDispatch } from '../../store/store'
-import { FavoriteIcon } from '../ui/icons'
+import { AppDispatch, IRootState } from '../../store/store'
+import { FavoriteIcon, LoadingIcon } from '../ui/icons'
+import { useEffect } from 'react'
+import { fetchPokemonTypes } from '../../store/slices/pokemonSlice'
+import pokeballIcon from '../../assets/pokeball.svg'
 
 function Item({
   href,
@@ -10,16 +13,23 @@ function Item({
   text,
 }: {
   href: string
-  icon: React.ReactElement
+  icon?: React.ReactElement
   text: string
 }) {
   return (
     <li>
       <Link
         to={href}
-        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group w-full"
+        className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white 
+        group w-full hover:bg-gray-100 dark:hover:bg-gray-700`}
       >
-        {icon}
+        {icon ?? (
+          <img
+            src={pokeballIcon}
+            alt="pokeball menu item icon"
+            className="h-6"
+          />
+        )}
         <span className="ms-3">{text}</span>
       </Link>
     </li>
@@ -28,6 +38,14 @@ function Item({
 
 function Menu() {
   const dispatch = useDispatch<AppDispatch>()
+  const { pokemonTypes, loadingPokemonTypes } = useSelector(
+    (state: IRootState) => state.pokemon
+  )
+
+  useEffect(() => {
+    dispatch(fetchPokemonTypes())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const handleOnClick = () => {
     dispatch(toggleDrawer())
@@ -41,6 +59,13 @@ function Menu() {
         text="Favorite Pokemons"
         icon={<FavoriteIcon status={true} />}
       />
+      {loadingPokemonTypes ? (
+        <LoadingIcon />
+      ) : (
+        pokemonTypes.map(({ name }) => (
+          <Item href={`/type/${name}`} text={name} key={name} />
+        ))
+      )}
     </ul>
   )
 }
