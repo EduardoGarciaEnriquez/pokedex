e0e519bbbeb5e21f0abbdfb4a87733e3f482b886
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-09-04 15:16:22 -0600

    Fix: improved drawer interaction when screen > md breakpoint.

--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -14,30 +14,29 @@ function Drawer() {
 
   return (
     <div
-      id="drawer-navigation"
-      className={`fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto bg-white w-full max-w-md dark:bg-gray-800 transition-transform duration-500 ${
+      className={`flex flex-row fixed top-0 left-0 z-40 h-screen w-full transition-transform duration-500 ${
         !isDrawerVisible && '-translate-x-full'
       }`}
-      aria-labelledby="drawer-navigation-label"
     >
-      <h5
-        id="drawer-navigation-label"
-        className="text-base font-semibold text-gray-500 uppercase dark:text-gray-400"
-      >
-        PokeMenu
-      </h5>
-      <button
-        onClick={handleOnClick}
-        type="button"
-        data-drawer-hide="drawer-navigation"
-        aria-controls="drawer-navigation"
-        className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
-      >
-        <CloseIcon />
-      </button>
-      <div className="py-4 overflow-y-auto">
-        <Menu />
+      <div className="h-full w-full max-w-md relative overflow-auto p-4 bg-white dark:bg-gray-800">
+        <h5 className="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">
+          PokeMenu
+        </h5>
+        <button
+          onClick={handleOnClick}
+          type="button"
+          className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+        >
+          <CloseIcon />
+        </button>
+        <div className="py-4 overflow-y-auto">
+          <Menu />
+        </div>
       </div>
+      <div
+        className="md:h-full md:w-full bg-white dark:bg-black opacity-80"
+        onClick={handleOnClick}
+      />
     </div>
   )
 }
