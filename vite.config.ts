d5b25ed9e01c03d77786940ca051cb6f858cee22
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-09-04 20:35:59 -0600

    Removed relative paths from vite.config.ts and tsconfig.app.json files

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-      '@layouts': '/src/layouts',
-      '@pages': '/src/pages',
-      '@assets': '/src/assets',
-    },
-  },
+  plugins: [react()]
 })
