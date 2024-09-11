c66fda7aba21b3941e144240f3af000913ad60b6
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-09-11 15:55:09 -0600

    Test: added utils wrapper to wrap any tested component in the store Provider.

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,15 @@
+import React, { PropsWithChildren } from 'react'
+import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+
+import { store } from '../store/store'
+
+export function renderWithProviders(
+  ui: React.ReactElement,
+  { ...renderOptions } = {}
+) {
+  function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
+    return <Provider store={store}>{children}</Provider>
+  }
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+}
